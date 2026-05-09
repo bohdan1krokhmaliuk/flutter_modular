6d260093b1481df897086749c06b40f724ec2668
bohdan krokhmaliuk <bohdan.krokhmaliuk@gmail.com>
2026-05-09 22:22:44 +0300

    analysis: enum-classes

--- core/design_system/lib/src/atoms/dimension.dart
+++ core/design_system/lib/src/atoms/dimension.dart
@@ -3,8 +3,17 @@ import 'package:flutter/material.dart';
 
 const dimen = _Dimension.standard;
 
-@immutable
-class _Dimension {
+enum _Dimension {
+  standard(
+    left: _DimensionSide.left,
+    top: _DimensionSide.top,
+    right: _DimensionSide.right,
+    bottom: _DimensionSide.bottom,
+    x: _DimensionSide.x,
+    y: _DimensionSide.y,
+    all: _DimensionSide.all,
+  );
+
   const _Dimension({
     required this.left,
     required this.top,
@@ -22,20 +31,102 @@ class _Dimension {
   final _DimensionSide x;
   final _DimensionSide y;
   final _DimensionSide all;
-
-  static const standard = _Dimension(
-    left: _DimensionSide.left,
-    top: _DimensionSide.top,
-    right: _DimensionSide.right,
-    bottom: _DimensionSide.bottom,
-    x: _DimensionSide.x,
-    y: _DimensionSide.y,
-    all: _DimensionSide.all,
-  );
 }
 
 @immutable
-class _DimensionSide {
+enum _DimensionSide {
+  all(
+    none: EdgeInsets.zero,
+    nano: EdgeInsets.all(s.nano),
+    micro: EdgeInsets.all(s.micro),
+    xxs: EdgeInsets.all(s.xxs),
+    xs: EdgeInsets.all(s.xs),
+    sm: EdgeInsets.all(s.sm),
+    md: EdgeInsets.all(s.md),
+    lg: EdgeInsets.all(s.lg),
+    xl: EdgeInsets.all(s.xl),
+    xxl: EdgeInsets.all(s.xxl),
+    xxxl: EdgeInsets.all(s.xxxl),
+  ),
+  left(
+    none: EdgeInsets.zero,
+    nano: EdgeInsets.only(left: s.nano),
+    micro: EdgeInsets.only(left: s.micro),
+    xxs: EdgeInsets.only(left: s.xxs),
+    xs: EdgeInsets.only(left: s.xs),
+    sm: EdgeInsets.only(left: s.sm),
+    md: EdgeInsets.only(left: s.md),
+    lg: EdgeInsets.only(left: s.lg),
+    xl: EdgeInsets.only(left: s.xl),
+    xxl: EdgeInsets.only(left: s.xxl),
+    xxxl: EdgeInsets.only(left: s.xxxl),
+  ),
+  top(
+    none: EdgeInsets.zero,
+    nano: EdgeInsets.only(top: s.nano),
+    micro: EdgeInsets.only(top: s.micro),
+    xxs: EdgeInsets.only(top: s.xxs),
+    xs: EdgeInsets.only(top: s.xs),
+    sm: EdgeInsets.only(top: s.sm),
+    md: EdgeInsets.only(top: s.md),
+    lg: EdgeInsets.only(top: s.lg),
+    xl: EdgeInsets.only(top: s.xl),
+    xxl: EdgeInsets.only(top: s.xxl),
+    xxxl: EdgeInsets.only(top: s.xxxl),
+  ),
+  right(
+    none: EdgeInsets.zero,
+    nano: EdgeInsets.only(right: s.nano),
+    micro: EdgeInsets.only(right: s.micro),
+    xxs: EdgeInsets.only(right: s.xxs),
+    xs: EdgeInsets.only(right: s.xs),
+    sm: EdgeInsets.only(right: s.sm),
+    md: EdgeInsets.only(right: s.md),
+    lg: EdgeInsets.only(right: s.lg),
+    xl: EdgeInsets.only(right: s.xl),
+    xxl: EdgeInsets.only(right: s.xxl),
+    xxxl: EdgeInsets.only(right: s.xxxl),
+  ),
+  bottom(
+    none: EdgeInsets.zero,
+    nano: EdgeInsets.only(bottom: s.nano),
+    micro: EdgeInsets.only(bottom: s.micro),
+    xxs: EdgeInsets.only(bottom: s.xxs),
+    xs: EdgeInsets.only(bottom: s.xs),
+    sm: EdgeInsets.only(bottom: s.sm),
+    md: EdgeInsets.only(bottom: s.md),
+    lg: EdgeInsets.only(bottom: s.lg),
+    xl: EdgeInsets.only(bottom: s.xl),
+    xxl: EdgeInsets.only(bottom: s.xxl),
+    xxxl: EdgeInsets.only(bottom: s.xxxl),
+  ),
+  x(
+    none: EdgeInsets.zero,
+    nano: EdgeInsets.symmetric(horizontal: s.nano),
+    micro: EdgeInsets.symmetric(horizontal: s.micro),
+    xxs: EdgeInsets.symmetric(horizontal: s.xxs),
+    xs: EdgeInsets.symmetric(horizontal: s.xs),
+    sm: EdgeInsets.symmetric(horizontal: s.sm),
+    md: EdgeInsets.symmetric(horizontal: s.md),
+    lg: EdgeInsets.symmetric(horizontal: s.lg),
+    xl: EdgeInsets.symmetric(horizontal: s.xl),
+    xxl: EdgeInsets.symmetric(horizontal: s.xxl),
+    xxxl: EdgeInsets.symmetric(horizontal: s.xxxl),
+  ),
+  y(
+    none: EdgeInsets.zero,
+    nano: EdgeInsets.symmetric(vertical: s.nano),
+    micro: EdgeInsets.symmetric(vertical: s.micro),
+    xxs: EdgeInsets.symmetric(vertical: s.xxs),
+    xs: EdgeInsets.symmetric(vertical: s.xs),
+    sm: EdgeInsets.symmetric(vertical: s.sm),
+    md: EdgeInsets.symmetric(vertical: s.md),
+    lg: EdgeInsets.symmetric(vertical: s.lg),
+    xl: EdgeInsets.symmetric(vertical: s.xl),
+    xxl: EdgeInsets.symmetric(vertical: s.xxl),
+    xxxl: EdgeInsets.symmetric(vertical: s.xxxl),
+  );
+
   const _DimensionSide({
     required this.none,
     required this.nano,
@@ -61,102 +152,4 @@ class _DimensionSide {
   final EdgeInsets xl;
   final EdgeInsets xxl;
   final EdgeInsets xxxl;
-
-  static const all = _DimensionSide(
-    none: EdgeInsets.zero,
-    nano: EdgeInsets.all(s.nano),
-    micro: EdgeInsets.all(s.micro),
-    xxs: EdgeInsets.all(s.xxs),
-    xs: EdgeInsets.all(s.xs),
-    sm: EdgeInsets.all(s.sm),
-    md: EdgeInsets.all(s.md),
-    lg: EdgeInsets.all(s.lg),
-    xl: EdgeInsets.all(s.xl),
-    xxl: EdgeInsets.all(s.xxl),
-    xxxl: EdgeInsets.all(s.xxxl),
-  );
-
-  static const left = _DimensionSide(
-    none: EdgeInsets.zero,
-    nano: EdgeInsets.only(left: s.nano),
-    micro: EdgeInsets.only(left: s.micro),
-    xxs: EdgeInsets.only(left: s.xxs),
-    xs: EdgeInsets.only(left: s.xs),
-    sm: EdgeInsets.only(left: s.sm),
-    md: EdgeInsets.only(left: s.md),
-    lg: EdgeInsets.only(left: s.lg),
-    xl: EdgeInsets.only(left: s.xl),
-    xxl: EdgeInsets.only(left: s.xxl),
-    xxxl: EdgeInsets.only(left: s.xxxl),
-  );
-
-  static const top = _DimensionSide(
-    none: EdgeInsets.zero,
-    nano: EdgeInsets.only(top: s.nano),
-    micro: EdgeInsets.only(top: s.micro),
-    xxs: EdgeInsets.only(top: s.xxs),
-    xs: EdgeInsets.only(top: s.xs),
-    sm: EdgeInsets.only(top: s.sm),
-    md: EdgeInsets.only(top: s.md),
-    lg: EdgeInsets.only(top: s.lg),
-    xl: EdgeInsets.only(top: s.xl),
-    xxl: EdgeInsets.only(top: s.xxl),
-    xxxl: EdgeInsets.only(top: s.xxxl),
-  );
-
-  static const right = _DimensionSide(
-    none: EdgeInsets.zero,
-    nano: EdgeInsets.only(right: s.nano),
-    micro: EdgeInsets.only(right: s.micro),
-    xxs: EdgeInsets.only(right: s.xxs),
-    xs: EdgeInsets.only(right: s.xs),
-    sm: EdgeInsets.only(right: s.sm),
-    md: EdgeInsets.only(right: s.md),
-    lg: EdgeInsets.only(right: s.lg),
-    xl: EdgeInsets.only(right: s.xl),
-    xxl: EdgeInsets.only(right: s.xxl),
-    xxxl: EdgeInsets.only(right: s.xxxl),
-  );
-
-  static const bottom = _DimensionSide(
-    none: EdgeInsets.zero,
-    nano: EdgeInsets.only(bottom: s.nano),
-    micro: EdgeInsets.only(bottom: s.micro),
-    xxs: EdgeInsets.only(bottom: s.xxs),
-    xs: EdgeInsets.only(bottom: s.xs),
-    sm: EdgeInsets.only(bottom: s.sm),
-    md: EdgeInsets.only(bottom: s.md),
-    lg: EdgeInsets.only(bottom: s.lg),
-    xl: EdgeInsets.only(bottom: s.xl),
-    xxl: EdgeInsets.only(bottom: s.xxl),
-    xxxl: EdgeInsets.only(bottom: s.xxxl),
-  );
-
-  static const x = _DimensionSide(
-    none: EdgeInsets.zero,
-    nano: EdgeInsets.symmetric(horizontal: s.nano),
-    micro: EdgeInsets.symmetric(horizontal: s.micro),
-    xxs: EdgeInsets.symmetric(horizontal: s.xxs),
-    xs: EdgeInsets.symmetric(horizontal: s.xs),
-    sm: EdgeInsets.symmetric(horizontal: s.sm),
-    md: EdgeInsets.symmetric(horizontal: s.md),
-    lg: EdgeInsets.symmetric(horizontal: s.lg),
-    xl: EdgeInsets.symmetric(horizontal: s.xl),
-    xxl: EdgeInsets.symmetric(horizontal: s.xxl),
-    xxxl: EdgeInsets.symmetric(horizontal: s.xxxl),
-  );
-
-  static const y = _DimensionSide(
-    none: EdgeInsets.zero,
-    nano: EdgeInsets.symmetric(vertical: s.nano),
-    micro: EdgeInsets.symmetric(vertical: s.micro),
-    xxs: EdgeInsets.symmetric(vertical: s.xxs),
-    xs: EdgeInsets.symmetric(vertical: s.xs),
-    sm: EdgeInsets.symmetric(vertical: s.sm),
-    md: EdgeInsets.symmetric(vertical: s.md),
-    lg: EdgeInsets.symmetric(vertical: s.lg),
-    xl: EdgeInsets.symmetric(vertical: s.xl),
-    xxl: EdgeInsets.symmetric(vertical: s.xxl),
-    xxxl: EdgeInsets.symmetric(vertical: s.xxxl),
-  );
 }
--- core/design_system/lib/src/atoms/radius.dart
+++ core/design_system/lib/src/atoms/radius.dart
@@ -11,7 +11,15 @@ const _round = 999.0;
 const radius = _Radius.standard;
 
 @immutable
-class _Radius {
+enum _Radius {
+  standard(
+    all: _RadiusSide.all,
+    top: _RadiusSide.top,
+    bottom: _RadiusSide.bottom,
+    left: _RadiusSide.left,
+    right: _RadiusSide.right,
+  );
+
   const _Radius({
     required this.all,
     required this.top,
@@ -25,18 +33,61 @@ class _Radius {
   final _RadiusSide bottom;
   final _RadiusSide left;
   final _RadiusSide right;
-
-  static const standard = _Radius(
-    all: _RadiusSide.all,
-    top: _RadiusSide.top,
-    bottom: _RadiusSide.bottom,
-    left: _RadiusSide.left,
-    right: _RadiusSide.right,
-  );
 }
 
 @immutable
-class _RadiusSide {
+enum _RadiusSide {
+  all(
+    none: BorderRadius.zero,
+    xs: BorderRadius.all(Radius.circular(_xs)),
+    sm: BorderRadius.all(Radius.circular(_sm)),
+    md: BorderRadius.all(Radius.circular(_md)),
+    lg: BorderRadius.all(Radius.circular(_lg)),
+    xl: BorderRadius.all(Radius.circular(_xl)),
+    xxl: BorderRadius.all(Radius.circular(_xxl)),
+    round: BorderRadius.all(Radius.circular(_round)),
+  ),
+  top(
+    none: BorderRadius.zero,
+    xs: BorderRadius.vertical(top: Radius.circular(_xs)),
+    sm: BorderRadius.vertical(top: Radius.circular(_sm)),
+    md: BorderRadius.vertical(top: Radius.circular(_md)),
+    lg: BorderRadius.vertical(top: Radius.circular(_lg)),
+    xl: BorderRadius.vertical(top: Radius.circular(_xl)),
+    xxl: BorderRadius.vertical(top: Radius.circular(_xxl)),
+    round: BorderRadius.vertical(top: Radius.circular(_round)),
+  ),
+  bottom(
+    none: BorderRadius.zero,
+    xs: BorderRadius.vertical(bottom: Radius.circular(_xs)),
+    sm: BorderRadius.vertical(bottom: Radius.circular(_sm)),
+    md: BorderRadius.vertical(bottom: Radius.circular(_md)),
+    lg: BorderRadius.vertical(bottom: Radius.circular(_lg)),
+    xl: BorderRadius.vertical(bottom: Radius.circular(_xl)),
+    xxl: BorderRadius.vertical(bottom: Radius.circular(_xxl)),
+    round: BorderRadius.vertical(bottom: Radius.circular(_round)),
+  ),
+  left(
+    none: BorderRadius.zero,
+    xs: BorderRadius.horizontal(left: Radius.circular(_xs)),
+    sm: BorderRadius.horizontal(left: Radius.circular(_sm)),
+    md: BorderRadius.horizontal(left: Radius.circular(_md)),
+    lg: BorderRadius.horizontal(left: Radius.circular(_lg)),
+    xl: BorderRadius.horizontal(left: Radius.circular(_xl)),
+    xxl: BorderRadius.horizontal(left: Radius.circular(_xxl)),
+    round: BorderRadius.horizontal(left: Radius.circular(_round)),
+  ),
+  right(
+    none: BorderRadius.zero,
+    xs: BorderRadius.horizontal(right: Radius.circular(_xs)),
+    sm: BorderRadius.horizontal(right: Radius.circular(_sm)),
+    md: BorderRadius.horizontal(right: Radius.circular(_md)),
+    lg: BorderRadius.horizontal(right: Radius.circular(_lg)),
+    xl: BorderRadius.horizontal(right: Radius.circular(_xl)),
+    xxl: BorderRadius.horizontal(right: Radius.circular(_xxl)),
+    round: BorderRadius.horizontal(right: Radius.circular(_round)),
+  );
+
   const _RadiusSide({
     required this.none,
     required this.xs,
@@ -56,59 +107,4 @@ class _RadiusSide {
   final BorderRadius xl;
   final BorderRadius xxl;
   final BorderRadius round;
-
-  static const all = _RadiusSide(
-    none: BorderRadius.zero,
-    xs: BorderRadius.all(Radius.circular(_xs)),
-    sm: BorderRadius.all(Radius.circular(_sm)),
-    md: BorderRadius.all(Radius.circular(_md)),
-    lg: BorderRadius.all(Radius.circular(_lg)),
-    xl: BorderRadius.all(Radius.circular(_xl)),
-    xxl: BorderRadius.all(Radius.circular(_xxl)),
-    round: BorderRadius.all(Radius.circular(_round)),
-  );
-
-  static const top = _RadiusSide(
-    none: BorderRadius.zero,
-    xs: BorderRadius.vertical(top: Radius.circular(_xs)),
-    sm: BorderRadius.vertical(top: Radius.circular(_sm)),
-    md: BorderRadius.vertical(top: Radius.circular(_md)),
-    lg: BorderRadius.vertical(top: Radius.circular(_lg)),
-    xl: BorderRadius.vertical(top: Radius.circular(_xl)),
-    xxl: BorderRadius.vertical(top: Radius.circular(_xxl)),
-    round: BorderRadius.vertical(top: Radius.circular(_round)),
-  );
-
-  static const bottom = _RadiusSide(
-    none: BorderRadius.zero,
-    xs: BorderRadius.vertical(bottom: Radius.circular(_xs)),
-    sm: BorderRadius.vertical(bottom: Radius.circular(_sm)),
-    md: BorderRadius.vertical(bottom: Radius.circular(_md)),
-    lg: BorderRadius.vertical(bottom: Radius.circular(_lg)),
-    xl: BorderRadius.vertical(bottom: Radius.circular(_xl)),
-    xxl: BorderRadius.vertical(bottom: Radius.circular(_xxl)),
-    round: BorderRadius.vertical(bottom: Radius.circular(_round)),
-  );
-
-  static const left = _RadiusSide(
-    none: BorderRadius.zero,
-    xs: BorderRadius.horizontal(left: Radius.circular(_xs)),
-    sm: BorderRadius.horizontal(left: Radius.circular(_sm)),
-    md: BorderRadius.horizontal(left: Radius.circular(_md)),
-    lg: BorderRadius.horizontal(left: Radius.circular(_lg)),
-    xl: BorderRadius.horizontal(left: Radius.circular(_xl)),
-    xxl: BorderRadius.horizontal(left: Radius.circular(_xxl)),
-    round: BorderRadius.horizontal(left: Radius.circular(_round)),
-  );
-
-  static const right = _RadiusSide(
-    none: BorderRadius.zero,
-    xs: BorderRadius.horizontal(right: Radius.circular(_xs)),
-    sm: BorderRadius.horizontal(right: Radius.circular(_sm)),
-    md: BorderRadius.horizontal(right: Radius.circular(_md)),
-    lg: BorderRadius.horizontal(right: Radius.circular(_lg)),
-    xl: BorderRadius.horizontal(right: Radius.circular(_xl)),
-    xxl: BorderRadius.horizontal(right: Radius.circular(_xxl)),
-    round: BorderRadius.horizontal(right: Radius.circular(_round)),
-  );
 }
