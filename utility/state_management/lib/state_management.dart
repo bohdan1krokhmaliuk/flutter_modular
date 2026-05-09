833ce853695f6d30759fedd91e5d3b9bdd3df37d
bohdan krokhmaliuk <bohdan.krokhmaliuk@gmail.com>
2026-05-09 15:47:19 +0300

    remove unnecessary export of bloc_monitoring_observer.dart

--- utility/state_management/lib/state_management.dart
+++ utility/state_management/lib/state_management.dart
@@ -1,4 +1,3 @@
 export 'package:state_management/src/annotations/bloc_event.dart';
 export 'package:state_management/src/annotations/bloc_state.dart';
-export 'package:state_management/src/bloc_monitoring_observer.dart';
 export 'package:state_management/src/di/di_initializer.dart';
