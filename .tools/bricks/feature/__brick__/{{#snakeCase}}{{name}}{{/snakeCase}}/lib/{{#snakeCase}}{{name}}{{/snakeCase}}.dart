73943b6aa126340ccad5e3252149001090aef0c7
bohdan krokhmaliuk <bohdan.krokhmaliuk@gmail.com>
2026-05-14 20:25:36 +0300

    fix brick

--- .tools/bricks/feature/__brick__/{{#snakeCase}}{{name}}{{/snakeCase}}/lib/{{#snakeCase}}{{name}}{{/snakeCase}}.dart
+++ .tools/bricks/feature/__brick__/{{#snakeCase}}{{name}}{{/snakeCase}}/lib/{{#snakeCase}}{{name}}{{/snakeCase}}.dart
@@ -1,3 +1,3 @@
+export 'package:{{#snakeCase}}{{name}}{{/snakeCase}}/src/configurator/{{#snakeCase}}{{name}}{{/snakeCase}}_configurator.dart';
 export 'package:{{#snakeCase}}{{name}}{{/snakeCase}}/src/entrypoint/{{#snakeCase}}{{name}}{{/snakeCase}}_entrypoint.dart';
 export 'package:{{#snakeCase}}{{name}}{{/snakeCase}}/src/navigator/{{#snakeCase}}{{name}}{{/snakeCase}}_navigator.dart';
-export 'package:{{#snakeCase}}{{name}}{{/snakeCase}}/src/navigator/{{#snakeCase}}{{name}}{{/snakeCase}}_configurator.dart';
